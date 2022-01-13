1b44a164857429ebe81156309be616982ef2f765
mikeroswell <mike.roswell@gmail.com>
2022-01-13 16:42:01 -0500

    starting to organize a repo for the Hill BEF paper

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # Hill-BEF
-Hill diversities other than richness might predict ecosystem fucntion better, which scaling exponent is best?
+
+Hill diversities other than richness might predict ecosystem function better --
+which scaling exponent is best?
+
+
+# Contributing authors
+
+Michael Roswell1 mroswell@umd.edu
+Rachael Winfree2 rwinfree@rutgers.edu
+Mark A. Genung3 mark.genung@louisiana.edu
+
+1. Dept. of Entomology, University of Maryland, College Park, MD 20742, USA
+1. Department of Ecology, Evolution & Natural Resources, Rutgers University, NJ
+08901, USA
+1. Dept of Biology, University of Louisiana, Lafayette, LA 70503, USA
+
+
+# Synopsis 
+
+Hill diversities have been promoted as robust metrics that integrate relative
+abundances more transparently into biodiversity measurement. Hill diversities
+summarize the distribution of relative abundances in an assemblage, and are
+governed by a control parameter that determines the relative emphasis on rare
+versus common species. Little empirical or theoretical work guides how to select
+the value of the control parameter, however, and most biodiversity studies use
+species richness, emphasizing the contribution of the rarest species to
+diversity. As an important justification for measuring and conserving
+biodiversity is protecting of the functions and services conferred by diverse
+ecosystems, here we ask how the relationship between biodiversity and ecosystem
+function (BEF) changes with the specification of species diversity through the
+Hill diversity control parameter. We tested which value of the control parameter
+produces the strongest BEF relationship in two large-scale, observational
+datasets, one on pollination by wild bees and the second on carbon storage by
+tropical trees. We measured the strength of the correlation between diversity
+and community-level as well as per-capita function. Unless richness is always
+more predictive than other Hill diversities for both full-community and
+per-capita function, adjusting the emphasis on rare versus common species may
+reveal mechanisms underlying the BEF relationship, and provide more
+readily-interpreted summaries of biodiversity change.
+
+# Organization
+```
+Hill-BEF
+| README.md
+| .gitignore
+|
+----- code
+|     |BEF_Hill_mg.R  # main analysis file
+| 
+----- data
+|     |a.list.rds # for now data in .rds file
+|      z.list.rds # for now data in .rds file
+|
+----- figures
+```
+
+
+
---
+++ code/BEF_Hill_mg.R
@@ -0,0 +1,170 @@
+n.datasets = 3 # how many separate datasets to analyze
+
+a.list = readRDS("/Users/Mark/Downloads/a.list.rds")
+z.list = readRDS("/Users/Mark/Downloads/z.list.rds")
+
+bef.r2 = vector("list", n.datasets)
+pcf.r2 = vector("list", n.datasets)
+Dq = vector("list", n.datasets)
+t.out = vector("list", n.datasets)
+c.out = vector("list", n.datasets)
+
+for(i in 1:n.datasets){
+  
+  # (1) load and format data
+  #####
+  
+  # load data (species (rows) by site (cols) of either abun or function data)
+  input.a = a.list[[i]] # abundance 
+  input.z = z.list[[i]] # function
+  
+  # convert column to row names (won't always be needed)
+  a = input.a; rownames(a) = input.a[,1]; a = a[,-1] # abundance
+  z = input.z; rownames(z) = input.z[,1]; z = z[,-1] # function
+  
+  # z is already total function in this case, so we need to do this (won't always be needed)
+  z = z/a; z = as.matrix(z); z[is.nan(z)] = 0
+  
+  # TH code
+  require(magrittr)
+  dim(a); dim(z)
+  
+  S = nrow(a) # gamma
+  N = ncol(a) # total site number
+  A = colSums(a) # site abundances
+  t = colSums(a*z) # site total function
+  p = apply(a, 2, function(x) x/sum(x)) # relative abundance matrix
+  cwm.func = colSums(p*z) # site cwm function
+  #####
+  
+  # (2) measure q-diversity
+  #####
+  
+  Ln = function(x) {ifelse(x != 0, log(x), 0)}
+  Exp = function(x, y) {ifelse(x != 0, x^y, 0)}
+  D.nab = function(z, q) {ifelse(q != 1,
+                                 sum(Exp(z/sum(z), q))^(1/(1 - q)),
+                                 exp(-sum((z/sum(z))*Ln(z/sum(z)))) )}
+  # example: here is how you get simpson diversity for each site
+  # apply(p, 2, D.nab, q = 2) 
+  # here is how you get a range of q-diversities for each site
+  # first define the range of q you are interested (change as desired)
+  qs = seq(0, 3, by=0.5) 
+  Dq[[i]] = sapply(qs, function(q) apply(a, 2, D.nab, q = q)) %>% t
+  # Dq # sites in columns, orders of q in rows
+  #####
+  
+  # for figures
+  #####
+  
+  # save total function and cwm function
+  t.out[[i]] = t
+  c.out[[i]] = cwm.func
+  
+  # r2
+  bef.r2[[i]] = apply(Dq[[i]], 1, function(x) {cor(log(x), log(t))^2})
+  pcf.r2[[i]] = apply(Dq[[i]], 1, function(x) {cor(log(x), log(cwm.func))^2})
+  #####
+}
+
+# main figs
+# sorry for using so many lines to do these figs
+# select species 1 to n.datasets
+# 1= P reptans, 2 = P tanacetifolia, 3 = M fistulosa
+se = 3
+
+q.cols = heat_hcl(length(qs)) # change if you want different colors
+
+# BEF
+ggplot() +
+  
+  stat_smooth(aes(x=Dq[[se]][1,],y=log(t.out[[se]])),method="lm",se=F,col=q.cols[1],size=2) +
+  stat_smooth(aes(x=Dq[[se]][2,],y=log(t.out[[se]])),method="lm",se=F,col=q.cols[2],size=2) +
+  stat_smooth(aes(x=Dq[[se]][3,],y=log(t.out[[se]])),method="lm",se=F,col=q.cols[3],size=2) +
+  stat_smooth(aes(x=Dq[[se]][4,],y=log(t.out[[se]])),method="lm",se=F,col=q.cols[4],size=2) +
+  stat_smooth(aes(x=Dq[[se]][5,],y=log(t.out[[se]])),method="lm",se=F,col=q.cols[5],size=2) +
+  stat_smooth(aes(x=Dq[[se]][6,],y=log(t.out[[se]])),method="lm",se=F,col=q.cols[6],size=2) +
+  stat_smooth(aes(x=Dq[[se]][7,],y=log(t.out[[se]])),method="lm",se=F,col=q.cols[7],size=2) +
+  
+  geom_point(aes(x=Dq[[se]][1,], y=log(t.out[[se]])), fill=q.cols[1], pch=21, 
+             stroke=0.2, size=4.5, alpha=0.5) +
+  geom_point(aes(x=Dq[[se]][2,], y=log(t.out[[se]])), fill=q.cols[2], pch=21, 
+             stroke=0.2, size=4.5, alpha=0.5) +
+  geom_point(aes(x=Dq[[se]][3,], y=log(t.out[[se]])), fill=q.cols[3], pch=21, 
+             stroke=0.2, size=4.5, alpha=0.5) +
+  geom_point(aes(x=Dq[[se]][4,], y=log(t.out[[se]])), fill=q.cols[4], pch=21, 
+             stroke=0.2, size=4.5, alpha=0.5) +
+  geom_point(aes(x=Dq[[se]][5,], y=log(t.out[[se]])), fill=q.cols[5], pch=21, 
+             stroke=0.2, size=4.5, alpha=0.5) +
+  geom_point(aes(x=Dq[[se]][6,], y=log(t.out[[se]])), fill=q.cols[6], pch=21, 
+             stroke=0.2, size=4.5, alpha=0.5) +
+  geom_point(aes(x=Dq[[se]][7,], y=log(t.out[[se]])), fill=q.cols[7], pch=21, 
+             stroke=0.2, size=4.5, alpha=0.5) +
+  
+  mytheme +
+  scale_x_continuous("Effective # of Species") +
+  scale_y_continuous("Total Function")
+
+# PCF
+ggplot() +
+  
+  stat_smooth(aes(x=Dq[[se]][1,], y=log(c.out[[se]])),method="lm",se=F,col=q.cols[1],size=2) +
+  stat_smooth(aes(x=Dq[[se]][2,], y=log(c.out[[se]])),method="lm",se=F,col=q.cols[2],size=2) +
+  stat_smooth(aes(x=Dq[[se]][3,], y=log(c.out[[se]])),method="lm",se=F,col=q.cols[3],size=2) +
+  stat_smooth(aes(x=Dq[[se]][4,], y=log(c.out[[se]])),method="lm",se=F,col=q.cols[4],size=2) +
+  stat_smooth(aes(x=Dq[[se]][5,], y=log(c.out[[se]])),method="lm",se=F,col=q.cols[5],size=2) +
+  stat_smooth(aes(x=Dq[[se]][6,], y=log(c.out[[se]])),method="lm",se=F,col=q.cols[6],size=2) +
+  stat_smooth(aes(x=Dq[[se]][7,], y=log(c.out[[se]])),method="lm",se=F,col=q.cols[7],size=2) +
+  
+  geom_point(aes(x=Dq[[se]][1,], y=log(c.out[[se]])), fill=q.cols[1], pch=21,
+             stroke=0.2,size=4.5,alpha=0.5) +
+  geom_point(aes(x=Dq[[se]][2,], y=log(c.out[[se]])), fill=q.cols[2], pch=21,
+             stroke=0.2,size=4.5,alpha=0.5) +
+  geom_point(aes(x=Dq[[se]][3,], y=log(c.out[[se]])), fill=q.cols[3], pch=21,
+             stroke=0.2,size=4.5,alpha=0.5) +
+  geom_point(aes(x=Dq[[se]][4,], y=log(c.out[[se]])), fill=q.cols[4], pch=21,
+             stroke=0.2,size=4.5,alpha=0.5) +
+  geom_point(aes(x=Dq[[se]][5,], y=log(c.out[[se]])), fill=q.cols[5], pch=21,
+             stroke=0.2,size=4.5,alpha=0.5) +
+  geom_point(aes(x=Dq[[se]][6,], y=log(c.out[[se]])), fill=q.cols[6], pch=21,
+             stroke=0.2,size=4.5,alpha=0.5) +
+  geom_point(aes(x=Dq[[se]][7,], y=log(c.out[[se]])), fill=q.cols[7], pch=21,
+             stroke=0.2,size=4.5,alpha=0.5) +
+  
+  mytheme +
+  scale_x_continuous("Effective # of Species") +
+  scale_y_continuous("Per-Capita Function")
+
+# these plots are for all three species so you don't need to pick one
+
+# r2 plot - BEF
+ggplot() +
+  
+  geom_line(aes(x=qs, y=bef.r2[[1]]), size=2) +
+  geom_point(aes(x=qs, y=bef.r2[[1]]), fill=q.cols, pch=21, stroke=1, size=8) +
+  
+  geom_line(aes(x=qs, y=bef.r2[[2]]), size=2) +
+  geom_point(aes(x=qs, y=bef.r2[[2]]), fill=q.cols, pch=21, stroke=1, size=8) +
+  
+  geom_line(aes(x=qs, y=bef.r2[[3]]), size=2) +
+  geom_point(aes(x=qs, y=bef.r2[[3]]), fill=q.cols, pch=21, stroke=1, size=8) +
+  
+  mytheme +
+  scale_x_continuous("q") +
+  scale_y_continuous("BEF r2")
+
+# r2 plot - PCF
+ggplot() +
+  
+  geom_line(aes(x=qs, y=pcf.r2[[1]]), size=2) +
+  geom_point(aes(x=qs, y=pcf.r2[[1]]), fill=q.cols, pch=21, stroke=1, size=8) +
+  
+  geom_line(aes(x=qs, y=pcf.r2[[2]]), size=2) +
+  geom_point(aes(x=qs, y=pcf.r2[[2]]), fill=q.cols, pch=21, stroke=1, size=8) +
+  
+  geom_line(aes(x=qs, y=pcf.r2[[3]]), size=2) +
+  geom_point(aes(x=qs, y=pcf.r2[[3]]), fill=q.cols, pch=21, stroke=1, size=8) +
+  
+  mytheme +
+  scale_x_continuous("q") +
+  scale_y_continuous("PCF r2")
