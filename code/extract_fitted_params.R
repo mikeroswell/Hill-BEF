9be87a1b4f5e18df592e416c165905c2a355fb84
mikeroswell <mike.roswell@gmail.com>
2022-06-15 08:16:53 -0400

    made more functional, allows backing out params

--- code/extract_fitted_params.R
+++ code/extract_fitted_params.R
@@ -5,6 +5,8 @@ library(furrr)
 library(tictoc)
 library(MeanRarity)
 
+source("code/format_BEF_data.R")
+
 # learn how to pull out neg. binomial params from a distribution
 ?fitdistr
 afit<-fitdistr(sample_infinite(fit_SAD()[[3]], 100)
@@ -12,14 +14,16 @@ afit<-fitdistr(sample_infinite(fit_SAD()[[3]], 100)
 
 glm(family ="gamma")
 
-province_params <- map_dfr(unique(lefcheck$Province), function(prov){
-  dat <- lefcheck %>% 
-    filter(Province == prov & Biomass>0) %>% 
-    drop_na()
-  print(length(dat$Abundance))
-  if(length(dat$Abundance) < 10){return(data.frame(Province = prov
-                                                  , meanSiteAb = sum(dat$Abundance)/length(dat$Abundance)
-                                                  , obsRich = length(unique(dat$SPECIES_NAME))
+# at least for now, this function only takes a df as its argument. 
+# this means that columns have to be named according to the lefcheck data
+# `Province` names a collection of sites
+# `Abundance` is abundance, `Biomass` is species-level EF
+# `SiteCode` names an individual site and `SPECIES_NAME` an individual sp. 
+par_getter <- function(dat){
+  if(length(dat$Abundance) < 10){return(data.frame(Province = dat$Province
+                                                   , meanSiteAb = sum(dat$Abundance)/length(dat$Abundance)
+                                                   , gammaRichObs = length(unique(dat$SPECIES_NAME))
+                                                   , n_sites = length(unique(dat$SiteCode))
                                                    , size_est = NA
                                                    , size_sd = NA
                                                    , mu_est = NA
@@ -27,34 +31,41 @@ province_params <- map_dfr(unique(lefcheck$Province), function(prov){
                                                    , EF_disp = NA
                                                    , EF_slope = NA))
   }
-
+  
   nbpar <- MASS::fitdistr(dat$Abundance, densfun = "Negative Binomial"
                           , lower=c(1e-5, 1e-5))
   lmDat<-dat %>% group_by(SiteCode) %>% summarize(Ab = sum(Abundance), EF = sum(Biomass))
   glmPar <- glm(lmDat$EF ~ log(lmDat$Ab), family = Gamma(link = "log")) 
   glmSum <- summary(glmPar)
-  return(data.frame(Province = prov
+  return(data.frame(Province = dat$Province
                     , meanSiteAb = sum(dat$Abundance)/length(dat$Abundance)
-                    , obsRich = length(unique(dat$SPECIES_NAME))
+                    , gammaRichObs = length(unique(dat$SPECIES_NAME))
+                    , n_sites = length(unique(dat$SiteCode))
                     , size_est = nbpar$estimate[1]
                     , size_sd = nbpar$sd[1]
                     , mu_est = nbpar$estimate[2]
                     , mu_sd = nbpar$sd[2]
                     , EF_disp = glmSum$dispersion
                     , EF_slope = glmSum$coefficients[[2]]))
+  }
+
+lefchecked <-  lefcheck %>% 
+  group_by(Province) %>% 
+  filter(n()>10) 
+province_params <- map_dfr(unique(lefchecked$Province), function(prov){
+  dat <- lefchecked %>% 
+    filter(Province == prov & Biomass>0) %>% 
+   
+    ungroup() %>% 
+    drop_na()
+  print(length(dat$Abundance))
+  par_getter(dat)
 })
+ 
 
 pdf("figures/simulation_params_from_empirical_data.pdf")
-pairs(province_params[,c(2,3,4, 6, 8, 9 )]
-      , gap = 1/1000
+pairs(province_params[,c(2,3,4,5, 7, 9, 10 )]
+      , gap = 1/6
       , upper.panel = NULL)
 dev.off()
-
-dprovince_params %>% pivot_longer(totAb, size_est, size_sd, mu_est, mu_sd, EF_disp, EF_slope
-                                 , names_to = "param"
-                                 , values_to = "val"
-                                 ) %>% 
-  ggplot(aes())
-  
-
 
