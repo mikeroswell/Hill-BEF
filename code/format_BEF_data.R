5b24ada12f32bbf9c0e3640e38738dcf474e1d54
mikeroswell <mike.roswell@gmail.com>
2022-08-20 14:25:35 -0400

    Got in a tangle with the bee data. Hoping MG can bail out.

--- code/format_BEF_data.R
+++ code/format_BEF_data.R
@@ -2,57 +2,52 @@
 library(tidyverse)
 
 
-# Bee data from Genung et al. 2022 Realistic Loss Order:
+# Bee data are from same dataset as Genung et al. 2022 Rare&Declining:
 # Visitation and estimated pollen deposition from Bees on 3 plant species at 
-# 25 sites set in a grid in Central New Jersey Sampled in 3 years
+# 25 sites set in a grid in Central New Jersey Sampled in 2 years. However,
+# visits and samples from the two years have been combined in this analysis and,
+# because "abundance" is measured as a per-minute visitation rate and sample
+# times weren't always identical across sites and years, the numbers are a bit
+# different from those in the Dryad repo https://doi.org/10.5061/dryad.qnk98sfkc
+
 # Although each plant species was sampled in the same 25 sites, we treat the 
 # samples from a given plant species as a distinct "metacommunity", as 
 # pollination of *that plant* is the ecosystem function in this context. 
-# For source data we used .rds files provided by M Genung. 
 
-
-# format the bee data
-a.list = readRDS("data/a.list.rds")
-z.list = readRDS("data/z.list.rds")
-# name the plants
-#################################################
-### NEED TO CHECK THE ORDER, THIS IS MADE UP!  ##
-##################################################
-pnames <- c( "Polemonium_reptans","Phacelia_tanacetifolia", "Monarda_fistulosa")
-names(a.list) <- pnames
-names(z.list) <- pnames
-
-a.list
-a.data <- a.list %>% 
-  lapply(function(x) {rownames(x) = x$X; x[,-1] %>% 
-  as.data.frame.matrix %>% 
-    as.matrix}) 
-z.t.data <- z.list %>% lapply(function(x) {rownames(x) = x$X; x[,-1] %>% 
-  as.data.frame.matrix %>% as.matrix}) 
-z.data = sapply(1:length(z.t.data),
-                function(se) {z = z.t.data[[se]]/a.data[[se]]
-                z[is.nan(z)] = 0; z})
-
-z.data
-a.data
-names(z.data) <- pnames
+# MG provided data source files. Raw data housed in Winfree Lab database at
+# Rutgers U.
 
 fix_bee <- function(x, value_name){pivot_longer(data.frame(x) %>% 
-                                      rownames_to_column("bee_species")
-                                    , cols = 2:last_col()
-                                    , names_to = c("site", "year")
-                                    , names_sep = "\\."
-                                    , values_to = value_name) 
-  }
-bee_abund <- purrr::map_dfr(a.data, function(x){fix_bee(x, value_name = "visit_rate")}, .id = "plant")
-bee_pcf <- purrr::map_dfr(z.data, function(x){fix_bee(x, value_name = "pollen_dep")}, .id = "plant")
+                                                  rename(bee_species = X)
+                                                , cols = 2:last_col()
+                                                , names_to = "site"
+                                                , values_to = value_name) 
+}
 
-bees_1819 <- left_join(bee_abund, bee_pcf) %>% 
-  group_by(plant, bee_species, site) %>% 
-  summarize(visit_rate = mean(visit_rate), pollen_dep = mean(pollen_dep)) %>% 
-  ungroup()
+beef <- list.files(path = "data", pattern = ".*_Apr22\\.csv")[1]
+pnames <- c( "Polemonium_reptans","Phacelia_tanacetifolia", "Monarda_fistulosa")
 
-write.csv(bees_1819, "data/bees_wrong.csv", row.names = FALSE)
+bee_dup <- map_dfr(list.files(path = "data", pattern = ".*_Apr22\\.csv"), function(beef){
+  dat <- read.csv(paste0("data/",beef))
+  value_name <- gsub( "_.*", "", beef)
+  plant_code <- gsub("(^.*_output_)(.*)(_Ap.*$)", "\\2", beef) 
+  plant_ID <- pnames[gsub("(^[a-z]{1})([a-z]*_)([a-z]{1})(.*$)"
+                          , "\\1\\3"
+                          , tolower(pnames))==plant_code]
+  return(data.frame(fix_bee(dat, value_name), plant_ID))
+})
+
+
+bee_dat <- left_join(bee_dup %>% filter(is.na(func)) %>% select(-func)
+                     , bee_dup %>% filter(is.na(abund)) %>% select(-abund)
+                     , by = c("bee_species", "site", "plant_ID") ) %>% 
+  select(plant_ID, site, bee_species, abund, ef = func) %>% 
+  arrange(plant_ID, site, bee_species)
+
+write.csv(bee_dat, "data/bef-scale_bees_taxonomy_issues.csv", row.names = FALSE)
+
+unique(bee_dat$bee_species) %>% sort
+
 # Fish data from Lefcheck et al. 2021 Nature Communications "Species Richness
 # and Identity..."
 # downloaded with script "code/Download_BEF_data.R"
